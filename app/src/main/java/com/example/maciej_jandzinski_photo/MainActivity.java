5f749935bbb7092acaa0e658bf81b34e8e6a2206
OGMaksiu <mateckimaksymilian@gmail.com>
2022-10-18 14:43:16 +0200

    added to opacity slide bar functionality

--- app/src/main/java/com/example/maciej_jandzinski_photo/MainActivity.java
+++ app/src/main/java/com/example/maciej_jandzinski_photo/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.maciej_jandzinski_photo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar opacityBar;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imageView);
+        opacityBar = (SeekBar) findViewById(R.id.sliderAlpha);
+        opacityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int opacity = 100;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                opacityBar.setAlpha(progresValue * 25);
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
     }
 }
