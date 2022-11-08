54ce28302d356cda45faeb006edb339591393ec7
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-08 12:18:58 +0100

    added funtionality of red slider

--- app/src/main/java/com/example/maciej_jandzinski_photo/MainActivity.java
+++ app/src/main/java/com/example/maciej_jandzinski_photo/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.maciej_jandzinski_photo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    SeekBar opacityBar;
+    SeekBar sliderR, sliderG, sliderB;
     ImageView image;
+    TextView color;
+    final int[] RValueInt = {0};
+    final int[] GValueInt = {0};
+    final int[] BValueInt = {0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         image = findViewById(R.id.imageView);
-        opacityBar = (SeekBar) findViewById(R.id.sliderAlpha);
-        opacityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int opacity = 100;
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-                opacityBar.setAlpha(progresValue * 25);
+        sliderR = findViewById(R.id.sliderRed);
+        sliderG = findViewById(R.id.sliderGreen);
+        sliderB = findViewById(R.id.sliderBlue);
 
+        sliderR.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int x = i;
+                RValueInt[0] = x;
+                ZmianaKoloru(RValueInt[0], GValueInt[0], BValueInt[0]);
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-
         });
-
+    }
+    public void ZmianaKoloru(int RValueInt, int GValueInt, int BValueInt) {
+        TextView text = findViewById(R.id.color);
+        text.setBackgroundColor(Color.rgb(RValueInt, GValueInt, BValueInt));
     }
 }
